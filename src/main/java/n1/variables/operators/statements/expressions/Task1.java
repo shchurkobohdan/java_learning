6113ab89b65017a92035fcf9f8c536b1691dfe34
shchurkobohdan <shchurko.b@gmail.com>
2018-06-14 10:48:39 +0300

    task 1

--- src/main/java/n1/variables/operators/statements/expressions/Task1.java
+++ src/main/java/n1/variables/operators/statements/expressions/Task1.java
@@ -3,24 +3,28 @@ package n1.variables.operators.statements.expressions;
 import java.util.Scanner;
 
 public class Task1 {
+
     public static void main(String[] args) {
         System.out.println("Enter a number and click Enter:");
         Scanner scaner = new Scanner(System.in);
-
+        String switchCondition = "";
 
         // first solution:
         int number = scaner.nextInt();
 
         if (number < 100){
+            switchCondition = "too less";
             System.out.println("too less");
         }
 
         if (number >= 100 && number <= 100500){
+            switchCondition = "its okay";
             System.out.println("its okay");
         }
 
         if (number > 100500){
-            System.out.println("too big");
+            switchCondition = "too big";
+           System.out.println("too big");
         }
         System.out.println("==================================");
 
@@ -35,6 +39,18 @@ public class Task1 {
         System.out.println("==================================");
 
         // third solution:
+        switch (switchCondition) {
+            case "too less":
+                System.out.println(switchCondition);
+                break;
+            case "its okay":
+                System.out.println(switchCondition);
+                break;
+            case "too big":
+                System.out.println(switchCondition);
+                break;
+        }
+        System.out.println("==================================");
 
     }
 }
